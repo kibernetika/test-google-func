583fefeb57365e6562dd6e32f7a76f06a6097db5
deni.ecybernetics@gmail.com <deni.ecybernetics@gmail.com>
2021-12-30 17:31:42 +0200

    Init

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-exports.helloWorld = (req, res) => {
+exports.myFuncHandler = (req, res) => {
     const message="<font color='blue'>СloudFunction of Denis Astahov!</font><br><b>App Version 1.1</b>";
     res.status(200).send(message);
 };
