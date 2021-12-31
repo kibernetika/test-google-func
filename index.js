c8b11ff3edfa37fe963db197769f9fd5abfd31c3
deni.ecybernetics@gmail.com <deni.ecybernetics@gmail.com>
2021-12-31 19:16:19 +0200

    deploy

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 exports.myFuncHandler = (req, res) => {
-    const message="<font color='blue'>GCP СloudFunction of Truepill !!!</font><br><b>App Version 1.3</b>";
+    const message="<font color='blue'>Cloud Function of Truepill !!!</font><br><b>App Version 1.3</b>";
     res.status(200).send(message);
 };
