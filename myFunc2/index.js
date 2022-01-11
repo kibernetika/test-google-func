eefd84749e5149d454ba8337804888616c68a80b
deni.ecybernetics@gmail.com <deni.ecybernetics@gmail.com>
2022-01-11 18:32:37 +0200

    replace yarn on npm

--- myFunc2/index.js
+++ myFunc2/index.js
@@ -1,4 +1,4 @@
 exports.myFuncHandler = (req, res) => {
-    const message="<font color='blue'>Cloud Function of Truepill !!! Func2</font><br><b>App Version 1.3</b>";
+    const message="<font color='blue'>Cloud Function of Truepill !!! Func2</font><br><b>App Version 1.4</b>";
     res.status(200).send(message);
 };
