d5e6522ca3ef0228e503c1c01d379850e944c2c4
deni.ecybernetics@gmail.com <deni.ecybernetics@gmail.com>
2022-01-17 15:03:40 +0200

    Add dotenv

--- functions/my-func/index.js
+++ functions/my-func/index.js
@@ -1,8 +1,13 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 exports.funcHandler = (req, res) => {
     console.log('_MY_TEST_1 ', process.env._MY_TEST_1);
     console.log('_MY_TEST_2 ', process.env._MY_TEST_2);
     console.log('MY_SECRET_KEY_2 ', process.env.MY_SECRET_KEY_2);
     console.log('MY_SECRET_KEY_3 ', process.env.MY_SECRET_KEY_3);
+    console.log('MY_SECRET_KEY_4 ', process.env.MY_SECRET_KEY_4);
     const message="<font color='blue'>Cloud Function of Truepill !!! Func1</font><br><b>App Version 1.9</b>";
     res.status(200).send(message);
 };
